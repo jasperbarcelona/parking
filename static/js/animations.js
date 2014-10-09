1ccc52f1b2f899bef51c2e4e868a121854577639
jasperbarcelona <barcelona.jasperoliver@gmail.com>
2014-10-10 02:11:57 +0800

    fi animations

--- static/js/animations.js
+++ static/js/animations.js
@@ -3,9 +3,6 @@ $(document).ready(function(){
 /*$('#intro').delay(4000).fadeOut(1500);*/
 
 $(".buttons").click(function(){
-	var height = $(window).height();
-	$(this).css({ "position": "absolute","left":"0"});
-   $(this).animate({height: "100%", width: "100%"}, "slow");
+	$(this).flip({direction:'lr',content:'Loading...',color:'#FFD700'})
 });
-
 });
