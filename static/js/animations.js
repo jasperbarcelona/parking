daca509cad2dc4d5cdd9c125d247d7054fb6e778
jasperbarcelona <barcelona.jasperoliver@gmail.com>
2014-10-10 02:41:58 +0800

    a

--- static/js/animations.js
+++ static/js/animations.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 
 $('#intro').delay(4000).fadeOut(1500);
+$('#content').delay(6000).fadeIn();
 
 $(".buttons").click(function(){
 	$(this).flip({direction:'lr',content:'Loading...',color:'#FFD700'})
