97c04c46c2272abbbf547c94362472a0e2f406bf
jasperbarcelona <barcelona.jasperoliver@gmail.com>
2014-10-10 02:32:47 +0800

    fix welcome screen

--- static/js/animations.js
+++ static/js/animations.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-/*$('#intro').delay(4000).fadeOut(1500);*/
+$('#intro').delay(4000).fadeOut(1500);
 
 $(".buttons").click(function(){
 	$(this).flip({direction:'lr',content:'Loading...',color:'#FFD700'})
