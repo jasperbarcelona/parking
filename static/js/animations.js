29f717b50e3fc48acd080d63a044edb519db1bcf
jasperbarcelona <barcelona.jasperoliver@gmail.com>
2014-10-19 18:14:54 +0800

    a

--- static/js/animations.js
+++ static/js/animations.js
@@ -4,16 +4,6 @@ $('#intro').delay(4000).fadeOut(1500);
 $('#content').delay(6000).fadeIn();
 
 
-$("#container").scroll(function () {
-    if ($("#container").scrollTop() > 40) {
-        $('#head').hide();
-    }
-    else{
-    	$('#head').show();
-    } 
-});
-
-
 $(".glyphicon-align-justify").click(function(){
 	$("#navigation").toggle();
 	$(this).toggleClass("clicked unclicked");
