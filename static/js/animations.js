1e977924a855768025e434c0752907687f7c2981
jasperbarcelona <barcelona.jasperoliver@gmail.com>
2014-10-19 15:53:54 +0800

    a

--- static/js/animations.js
+++ static/js/animations.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 $('#intro').delay(4000).fadeOut(1500);
 $('#content').delay(6000).fadeIn();
 
-$(".outerLayer").click(function(){
-	$(this).flip({direction:'lr',color:'#FFD700'})
+$(".glyphicon-align-justify").click(function(){
+	$("#navigation").toggle({ origin: ["top", "right"] });
 });
 });
