af959bdbf2849ba638a9438a9e954525cde3149c
jasperbarcelona <barcelona.jasperoliver@gmail.com>
2014-10-19 17:49:06 +0800

    a

--- static/js/animations.js
+++ static/js/animations.js
@@ -3,14 +3,22 @@ $(document).ready(function(){
 $('#intro').delay(4000).fadeOut(1500);
 $('#content').delay(6000).fadeIn();
 
+
+$("#container").scroll(function () {
+    if ($("#container").scrollTop() > 0) {
+        $('#head').slideUp();
+    }
+    else{
+    	$('#head').slideDown();
+    } 
+});
+
+
 $(".glyphicon-align-justify").click(function(){
 	$("#navigation").toggle();
 	$(this).toggleClass("clicked unclicked");
 	$("#container").toggleClass("containerClicked containerUnclicked");
-
-
-
-
 });
+
 
 });
