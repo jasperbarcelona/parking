cabf746874b45e7f5e6db0dcb38fa6d6503056bc
jasperbarcelona <barcelona.jasperoliver@gmail.com>
2014-10-19 16:40:57 +0800

    overlay

--- static/js/animations.js
+++ static/js/animations.js
@@ -4,6 +4,13 @@ $('#intro').delay(4000).fadeOut(1500);
 $('#content').delay(6000).fadeIn();
 
 $(".glyphicon-align-justify").click(function(){
-	$("#navigation").toggle({ origin: ["top", "right"] });
+	$("#navigation").toggle();
+	$(this).toggleClass("clicked unclicked");
+	$("#container").toggleClass("containerClicked containerUnclicked");
+
+
+
+
 });
+
 });
