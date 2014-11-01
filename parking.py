4eab0c173d1a8c0d1e2285a90c3cd8660f50eb61
jasperbarcelona <barcelona.jasperoliver@gmail.com>
2014-11-02 06:00:21 +0800

    add color schemes

--- parking.py
+++ parking.py
@@ -7,7 +7,7 @@ app = flask.Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def facebook_login():
-    return flask.render_template('index.html',data="hello")
+    return flask.render_template('index.html',scheme="light")
 
 if __name__ == '__main__':
     app.debug = True
