1f06ead55f58d973ac3b071dd33daed35b6c8f61
jasperbarcelona <barcelona.jasperoliver@gmail.com>
2014-11-01 16:05:26 +0800

    a

--- static/js/animations.js
+++ static/js/animations.js
@@ -9,6 +9,22 @@ var height = $(window).height()-80;
 $("#container").css("height",height);
 
 
+$("#container").scroll(function(){
+	$("#head").css("display","none");
+	$("#container").css("top","0");
+	$("#container").css("height","100%");
+	$("#container").css("border","1px solid red");
+	$("#search").animate({width: "95%"});
+	$("#search").css("position", "fixed");
+	$("#search").css("top", "0");
+	$("#search").css("left", "0");
+	$("#search").css("right", "0");
+	$("#search").css("margin-right", "auto");
+	$("#search").css("margin-left", "auto");
+	$(".places").css("margin-top", "80");
+	$(".destinations").css("margin-top", "-22");
+});
+
 
 $(window).resize(function(){
 	var height = $(window).height()-80;
@@ -16,9 +32,10 @@ $(window).resize(function(){
 });
 
 
-$(".glyphicon-align-justify").click(function(){
-	$("#navigation").slideToggle();
-	$(this).toggleClass("clicked unclicked");
+$("#toggleDown").click(function(){
+	$("#navigation").slideDown(1500);
+	$(this).css("display","none");
+	$("#toggleUp").show();
 	$("#container").toggleClass("containerClicked containerUnclicked");
 });
 
