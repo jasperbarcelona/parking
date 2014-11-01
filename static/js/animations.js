8e98a2f2c8b729c32042d3abf5fb4e118bf91749
jasperbarcelona <barcelona.jasperoliver@gmail.com>
2014-11-01 16:54:55 +0800

    a

--- static/js/animations.js
+++ static/js/animations.js
@@ -9,19 +9,6 @@ $(window).load(function() {
 $("#container").css("height",height);*/
 
 
-$(window).scroll(function(){
-	$("#head").slideUp();
-	$("#head").css("display","none");
-	$("#search").css("position","fixed");
-	$("#search").css("left","0");
-	$("#search").css("right","0");
-	$("#search").css("margin-left","auto");
-	$("#search").css("margin-right","auto");
-	$("#search").animate({width:"95%",top:"0"},500);
-
-
-});
-
 
 /*$(window).resize(function(){
 	var height = $(window).height()-80;
