bcf61eb380a5f6443f61c147c942ecc6de5a1b96
jasperbarcelona <barcelona.jasperoliver@gmail.com>
2014-11-03 00:42:24 +0800

    a

--- static/js/animations.js
+++ static/js/animations.js
@@ -9,29 +9,30 @@ var height = $(window).height()-60;
 $("#container").css("height",height);
 
 
-
 $(window).resize(function(){
 	var height = $(window).height()-60;
 	$("#container").css("height",height);
 });
 
+$(".rows").on('click', function() {
+	$(this).animate({"margin-left":"120%"});
+});
+
 
 $("#parking").on('click', function() {
 	var $this = jQuery(this);
-        if ($this.data('activated')) return false;  // Pending, return
+    if ($this.data('activated')) return false;  // Pending, return
 
-        $this.data('activated', true);
-        setTimeout(function() {
-            $this.data('activated', false)
-        }, 500); // Freeze for 500ms
-        
+    $this.data('activated', true);
+    setTimeout(function() {
+        $this.data('activated', false)
+    }, 500); // Freeze for 500ms
+
 	$("#navigation").stop().slideToggle();
 	$(".toggleUnclicked").switchClass( "toggleUnclicked", "toggleClicked" );
 	$(".toggleClicked").switchClass( "toggleClicked", "toggleUnclicked" );
 	$("#container").toggleClass("containerClicked containerUnclicked");
-	
+
 	return false; 
 });
-
-
 });
