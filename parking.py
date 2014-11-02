7a05feea06319cdeba0e24203774da99dbf41507
jasperbarcelona <barcelona.jasperoliver@gmail.com>
2014-11-02 17:32:21 +0800

    a

--- parking.py
+++ parking.py
@@ -7,7 +7,7 @@ app = flask.Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def facebook_login():
-    return flask.render_template('index.html',scheme="light")
+    return flask.render_template('index.html',scheme="orange")
 
 if __name__ == '__main__':
     app.debug = True
