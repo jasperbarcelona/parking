c3f8a7bfa93077b1f398cd71aff9bca8cbbdd849
jasperbarcelona <barcelona.jasperoliver@gmail.com>
2014-12-07 17:01:17 +0800

    a

--- static/js/animations.js
+++ static/js/animations.js
@@ -69,6 +69,21 @@ $("#changeTheme").on('click', function() {
 });
 
 
+var mousewheelevt = (/Firefox/i.test(navigator.userAgent)) ? "DOMMouseScroll" : "mousewheel" //FF doesn't recognize mousewheel as of FF3.x
+$('#container').bind(mousewheelevt, function(e){
+
+    var evt = window.event || e //equalize event object     
+    evt = evt.originalEvent ? evt.originalEvent : evt; //convert to originalEvent if possible               
+    var delta = evt.detail ? evt.detail*(-10) : evt.wheelDelta //check for detail first, because it is used by Opera and FF
+
+    if(delta > 0) {
+        $("#head").animate({"top":"0px"});
+    }
+    else{
+        $("#head").animate({"top":"-60px"});
+    }   
+});
+
 
 
 });
