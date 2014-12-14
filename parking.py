4a55eb5811a281feb5941b8f5e2c0624ec9305d5
jasperbarcelona <barcelona.jasperoliver@gmail.com>
2014-12-14 20:56:41 +0800

    a

--- parking.py
+++ parking.py
@@ -2,6 +2,10 @@ import flask, flask.views
 from flask import url_for, request, session, redirect
 from jinja2 import environment, FileSystemLoader
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext import admin
+from flask.ext.admin.contrib import sqla
+from flask.ext.admin.contrib.sqla import ModelView
+from flask.ext.admin import Admin, BaseView, expose
 from flask import render_template, request
 from flask import session, redirect
 from flask_oauth import OAuth
@@ -15,6 +19,13 @@ FACEBOOK_APP_ID = '539244542865228'
 FACEBOOK_APP_SECRET = 'efaee0037f9320831895a5e9aa4d1bc6'
 
 oauth = OAuth()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    facebook_id = db.Column(db.String(64), unique=True)
+    first_name = db.Column(db.String(64))
+    last_name = db.Column(db.String(64))
+    join_date = db.Column(db.String(64))
 
 facebook = oauth.remote_app('facebook',
     base_url='https://graph.facebook.com/',
@@ -30,9 +41,11 @@ facebook = oauth.remote_app('facebook',
 def get_facebook_token():
     return session.get('facebook_token')
 
+
 def pop_login_session():
     session.pop('logged_in', None)
     session.pop('facebook_token', None)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -53,21 +66,36 @@ def facebook_login():
     return facebook.authorize(callback=url_for('facebook_authorized',
         next=request.args.get('next'), _external=True))
 
+
 @app.route("/facebook_authorized")
 @facebook.authorized_handler
 def facebook_authorized(resp):
     next_url = request.args.get('next') or url_for('index')
+
     if resp is None or 'access_token' not in resp:
         return redirect(next_url)
     
     session['signedin'] = True
     session['facebook_token'] = (resp['access_token'], '')
     data = facebook.get('/me').data
+
     if 'id' in data and 'name' in data:
         session['facebookId'] = data['id']
         session['firstname'] = data['first_name']
         session['lastname'] = data['last_name']
+
+        if not user.query.filter_by(facebook_id=session['facebookId']).first():
+            register = User(facebook_id=session['facebookId'], first_name=session['firstname'],
+            last_name=session['lastname'], join_date=time.strftime('%b %d, %H:%S %p'))
+            db.session.add(register)
+            db.session.commit()
     return redirect(next_url)
+
+
+class IngAdmin(sqla.ModelView):
+    column_display_pk = True
+admin = Admin(app)
+admin.add_view(IngAdmin(User, db.session))
 
 
 @app.route('/changetheme', methods=['GET', 'POST'])
@@ -86,6 +114,7 @@ def next_page():
 def logout():
     session.clear()
     return redirect('/login')
+
 
 if __name__ == '__main__':
     app.debug = True
